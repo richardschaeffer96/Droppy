5d4f80a6ca06d35f381bac61bcbe29bc8a319011
Richard Schäffer <richard_schaeffer@web.de>
2019-06-09 16:39:18 +0200

    first try of start screen for debuging

--- app/src/main/java/com/example/deinvirtuellerfreund/MainActivity.java
+++ app/src/main/java/com/example/deinvirtuellerfreund/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.deinvirtuellerfreund;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button start_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton start_button = (ImageButton)findViewById(R.id.start_button);
+        start_button.setImageResource(R.drawable.start_button);
     }
 }
